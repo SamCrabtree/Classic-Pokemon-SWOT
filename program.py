6e30d54022ccedab1e544649c55a8f052cf6a2b9
Sam Crabtree <samuel.crabtree@protonmail.com>
2022-02-14 19:46:31 -0500

    Switched the Test program file to the active.
    Tested system and created a master loop.

--- program.py
+++ program.py
@@ -3,22 +3,52 @@ import pandas as pd
 
 entries = os.listdir('data/')
 
-print('Please Select the file you wish to review...\n')
+## MAIN PROGRAM FUNCTION
+def project_program():
+  print('Please Select the file you wish to review...\n')
 
-for entry in entries:
-    print(entry)
-
-selection = input('\n Please enter the name of the file you wish to review... ')
-
-if selection in entries:
-  print("\n You selected, " + selection + ". Is that correct?")
-else:
-    print('\n There is no file by that name. Please check your spelling and try again.')
-
-url = "data/" + selection
-
-df = pd.read_csv(url)
+## DISPLAYS FILES IN "DATA" FOLDER
+  for entry in entries:
+     print(entry)
 
 
-print(url)
-print(df)
+  selection = input('\n Please enter the name of the file you wish to review... ')
+
+##CREATES URL BASED ON FILE SELECTION
+  url = "data/" + selection
+  
+##CONFIRMS YOU CHOSE A VALID/CORRECT FILE
+
+  if selection in entries:
+    print("\n You selected, " + selection + ". Is that correct?")
+    confirmation = input("\nYes or No? ")
+
+  else:
+    print('\n There is no file by that name. Please check your spelling.')
+    confirmation = input("\nPress Enter To Try Again")
+    project_program()
+
+    
+## IF SELECTION IS VALID, THIS VARIABLE READS THE CSV FILE
+
+  df = pd.read_csv(url)
+
+##IF USER CONFIRMS THEIR SELECTION, IT THEN DISPLAYS THE CONTENTS OF THE FILE
+
+  if confirmation.lower() == 'yes':
+    print("\n Now displaying the contents of " + selection + ":\n")
+    print(df)
+    next = input("\n Would you like to like to view another file? Yes/No? ")
+    if next.lower() == 'yes':
+      project_program()
+    else:
+      exit()
+
+##IF THE USER CONFIRMS THAT THE SELECTION IS 
+  else: 
+    project_program()
+
+
+
+project_program()
+
--- program_test.py
+++ program_test.py
@@ -3,48 +3,22 @@ import pandas as pd
 
 entries = os.listdir('data/')
 
-## MAIN PROGRAM FUNCTION
-def project_program():
-  print('Please Select the file you wish to review...\n')
+print('Please Select the file you wish to review...\n')
 
-## DISPLAYS FILES IN "DATA" FOLDER
-  for entry in entries:
-     print(entry)
+for entry in entries:
+    print(entry)
 
+selection = input('\n Please enter the name of the file you wish to review... ')
 
-  selection = input('\n Please enter the name of the file you wish to review... ')
+if selection in entries:
+  print("\n You selected, " + selection + ". Is that correct?")
+else:
+    print('\n There is no file by that name. Please check your spelling and try again.')
 
-##CREATES URL BASED ON FILE SELECTION
-  url = "data/" + selection
-  
+url = "data/" + selection
 
-  if selection in entries:
-    print("\n You selected, " + selection + ". Is that correct?")
-    confirmation = input("Yes or No? ")
-    
-  else:
-    print('\n There is no file by that name. Please check your spelling.')
-    confirmation = input("Press Enter To Try Again")
-    project_program()
-
-    
-
-  df = pd.read_csv(url)
+df = pd.read_csv(url)
 
 
-  if confirmation.lower() == 'yes':
-    print(url)
-    print(df)
-    next = input("Would you like to like to view another file? Yes/No? ")
-    if next.lower() == 'yes':
-      project_program()
-    else:
-      exit()
-
-  else: 
-    project_program()
-
-
-
-project_program()
-
+print(url)
+print(df)
